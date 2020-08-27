f1c20ae79451e82848fee501e56b25af324f1e53
EC2 Default User <ec2-user@ip-172-31-37-151.us-west-2.compute.internal>
2020-08-27 14:08:08 +0000

    Added Yaml Check Point

---
+++ checkYaml.sh
@@ -0,0 +1,13 @@
+FILES=$(ls *.yaml)
+for file in $FILES
+do
+  echo "Validating $file"
+  python -c 'import yaml, sys; yaml.safe_load(sys.stdin)' < $file
+  statuscode=$(echo $?)
+  if [ $statuscode -eq 0 ]
+  then
+    echo "$file was proper yaml file"
+  else
+    echo "$file is not in proper yaml format. Please modify it"
+  fi
+done
